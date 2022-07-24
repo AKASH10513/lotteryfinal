a45dba2bf5728d1e6dc2a125f26dfc4da41e5201
AKASH10513 <88180327+AKASH10513@users.noreply.github.com>
2022-07-24 17:59:28 +0530

    Add files via upload
    FINALISED LOTTERY SYSTEM

---
+++ script.js
@@ -0,0 +1,16 @@
+
+    var typed = new Typed(".typing", {
+        strings: ["PLAY ","WIN","DREAM BIG"],
+        typeSpeed: 150,
+        backSpeed: 60,
+        loop: 0
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Student", "Web-Developer", "Technical content writer", "UI Designer", "Competitive Programmer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+
